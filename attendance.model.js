e88a3619db149f7b9902ed078962cf8621f3c1b2
Yash Tomar <yash12345y@gmail.com>
2025-09-10 03:32:58 +0530

    Updated feature/fix

--- attendance.model.js
+++ attendance.model.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const attendanceSchema = new mongoose.Schema({
-    // This will link the attendance record back to the original registration
     registrationId: {
         type: String,
         required: true,
-        ref: 'Registration' // Creates a reference to the Registration model
+        ref: 'Registration' 
     },
     fullName: {
         type: String,
@@ -13,12 +12,10 @@ const attendanceSchema = new mongoose.Schema({
     },
     checkedInAt: {
         type: Date,
-        default: Date.now // Automatically records the time of check-in
+        default: Date.now 
     }
 });
 
-// To prevent duplicate check-ins, this ensures a single registrationId can only be logged once.
-// If you want to allow multiple check-ins per person, you can remove this index.
 attendanceSchema.index({ registrationId: 1 }, { unique: true });
 
 const Attendance = mongoose.model('Attendance', attendanceSchema);
